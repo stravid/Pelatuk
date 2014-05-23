5769a956bf8f4b34ff0863ea634a20d6d942f415
David Strauß <mail@stravid.com>
2014-05-23 12:50:29 +0200

    Implement last command and dynamic target

--- plugin_one.py
+++ plugin_one.py
@@ -1,7 +1,9 @@
 import sublime, sublime_plugin, os, re
 
+last_command = ""
+
 def applescript_path():
-  return sublime.packages_path() + "/plugin_one/sendCommand.scpt"
+  return sublime.packages_path() + "/plugin_one/" + sublime.load_settings('plugin_one.sublime-settings').get('target') + ".scpt"
 
 def run_applescript(view, command):
   view.window().run_command("exec", {
@@ -22,6 +24,7 @@ class BaseTestCommand:
 
 class RunCursorPositionCommand(sublime_plugin.TextCommand, BaseTestCommand):
   def run(self, edit):
+    global last_command
     pattern = re.compile("test '(.*)' do")
     region = self.view.sel()[0]
     line = self.view.full_line(region)
@@ -35,17 +38,28 @@ class RunCursorPositionCommand(sublime_plugin.TextCommand, BaseTestCommand):
       if re.search(pattern, line_above_content):
         test_name = re.search(pattern, line_above_content).groups()[0]
         command = "clear; cd " + self.directory_of_file() + "; bundle exec rake test TEST=" + self.view.file_name() + " TESTOPTS=\"--name=test_" + test_name.replace(' ', '_') + "\""
+        last_command = command
         run_applescript(self.view, command)
 
         break
 
 class RunCurrentFileCommand(sublime_plugin.TextCommand, BaseTestCommand):
   def run(self, edit):
+    global last_command
+
     if "_test.rb" in self.file_name():
       command = "clear; cd " + self.directory_of_file() + "; bundle exec rake test TEST=" + self.view.file_name()
+      last_command = command
       run_applescript(self.view, command)
 
 class RunEverythingCommand(sublime_plugin.TextCommand, BaseTestCommand):
   def run(self, edit):
     command = "clear; cd " + self.directory_of_file() + "; bundle exec rake test"
     run_applescript(self.view, command)
+
+class RunLastCommand(sublime_plugin.TextCommand, BaseTestCommand):
+  def run(self, edit):
+    global last_command
+
+    if last_command.__len__() > 0:
+      run_applescript(self.view, last_command)
