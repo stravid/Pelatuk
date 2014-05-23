fc2a9657ef8013b61c4058bbf8cfe49846e8ed55
David Strauß <mail@stravid.com>
2014-05-23 11:36:33 +0200

    Refactor commands and reuse code

--- plugin_one.py
+++ plugin_one.py
@@ -1,92 +1,51 @@
 import sublime, sublime_plugin, os, re
 
-call_counter = 0
-
 def applescript_path():
   return sublime.packages_path() + "/plugin_one/sendCommand.scpt"
 
-class RunCursorPositionCommand(sublime_plugin.TextCommand):
+def run_applescript(view, command):
+  view.window().run_command("exec", {
+    "cmd": [
+      "osascript",
+      applescript_path(),
+      "plugin_one",
+      command
+    ]
+  })
+
+class BaseTestCommand:
+  def file_name(self):
+    return os.path.basename(self.view.file_name())
+
+  def directory_of_file(self):
+    return os.path.dirname(self.view.file_name())
+
+class RunCursorPositionCommand(sublime_plugin.TextCommand, BaseTestCommand):
   def run(self, edit):
-    global call_counter
-    call_counter += 1
-
-    print call_counter
-    print "RunCursorPositionCommand"
-
-    file_name = os.path.basename(self.view.file_name())
-    directory_of_file = os.path.dirname(self.view.file_name())
-
     pattern = re.compile("test '(.*)' do")
     region = self.view.sel()[0]
     line = self.view.full_line(region)
     line_content = self.view.substr(line)
 
-
     lines_above = reversed(self.view.lines(sublime.Region(0, line.b)))
-
-    # print line
-    # print lines_above
 
     for line_above in lines_above:
       line_above_content = self.view.substr(line_above)
 
-      # print line_above_content
-
       if re.search(pattern, line_above_content):
         test_name = re.search(pattern, line_above_content).groups()[0]
-
-        # print test_name
-
-        self.view.window().run_command("exec", {
-          "cmd": [
-            "osascript",
-            applescript_path(),
-            "run_cursor_position",
-            "clear; cd " + directory_of_file + "; bundle exec rake test TEST=" + self.view.file_name() + " TESTOPTS=\"--name=test_" + test_name.replace(' ', '_') + "\""
-          ]
-        })
+        command = "clear; cd " + self.directory_of_file() + "; bundle exec rake test TEST=" + self.view.file_name() + " TESTOPTS=\"--name=test_" + test_name.replace(' ', '_') + "\""
+        run_applescript(self.view, command)
 
         break
-    #   print line_above
 
-    # if re.search(pattern, line_content) and False:
-    #   test_name = re.search(pattern, line_content).groups()[0]
-    #   print 'YEAH'
-    #   print test_name
-
-    #   self.view.window().run_command("exec", {
-    #     "cmd": [
-    #       "osascript",
-    #       applescript_path(),
-    #       "run_cursor_position",
-    #       "clear; cd " + directory_of_file + "; bundle exec rake test TEST=" + self.view.file_name() + " TESTOPTS=\"--name=test_" + test_name.replace(' ', '_') + "\""
-    #     ]
-    #   })
-
-class RunCurrentFileCommand(sublime_plugin.TextCommand):
+class RunCurrentFileCommand(sublime_plugin.TextCommand, BaseTestCommand):
   def run(self, edit):
-    file_name = os.path.basename(self.view.file_name())
-    directory_of_file = os.path.dirname(self.view.file_name())
+    if "_test.rb" in self.file_name():
+      command = "clear; cd " + self.directory_of_file() + "; bundle exec rake test TEST=" + self.view.file_name()
+      run_applescript(self.view, command)
 
-    if "_test.rb" in file_name:
-      self.view.window().run_command("exec", {
-        "cmd": [
-          "osascript",
-          applescript_path(),
-          "run_cursor_position",
-          "clear; cd " + directory_of_file + "; bundle exec rake test TEST=" + self.view.file_name()
-        ]
-      })
-
-class RunEverythingCommand(sublime_plugin.TextCommand):
+class RunEverythingCommand(sublime_plugin.TextCommand, BaseTestCommand):
   def run(self, edit):
-    directory_of_file = os.path.dirname(self.view.file_name())
-
-    self.view.window().run_command("exec", {
-      "cmd": [
-        "osascript",
-        applescript_path(),
-        "run_cursor_position",
-        "clear; cd " + directory_of_file + "; bundle exec rake test"
-      ]
-    })
+    command = "clear; cd " + self.directory_of_file() + "; bundle exec rake test"
+    run_applescript(self.view, command)
